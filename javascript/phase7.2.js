110b658ef2a3b0d05bec06a62d8029557b33b813
Christopher Moreau <moreauchristopher7@gmail.com>
2022-08-19 11:56:32 +0200

    modif phase7.2.js

--- javascript/phase7.2.js
+++ javascript/phase7.2.js
@@ -1,7 +1,8 @@
 // exercice phase7.2
 let a= window.prompt("Quel est ton année de naissance ?");
-var age= 18;
-(age>=18) ? console.log("vous etes majeur") : console.log("vous etes mineur");
+if n->18
+
+
 
 
 
