918468839efc3672b9ee9f6ac6e254119a539fc1
Christopher Moreau <moreauchristopher7@gmail.com>
2022-08-24 16:52:53 +0200

    modif phase 7.3.js

--- javascript/phase 7.3.js
+++ javascript/phase 7.3.js
@@ -1,4 +1,39 @@
 ///////////////////////////////////////////////////////////phase 7.3/////////////////////////////////////////////////////////////////////////////////////////////////////////
-let a= window.prompt ("saisie d'un opérateur +,-,*,/")
+let a= window.prompt ("saisir un nombre");
+let b= window.prompt ("saisir un deuxieme nombre");
+let o= window.prompt ("saisir un operateur");
+let r
+if ((o!="+") && (o!="-") && (o!="*") && (o!="/"))
+{
+    alert("opérateur erroné");
+}
+else if ((o=="/")&& (b==0)){
+    alert ("erreur division par 0")
+}
 
+else 
+{
+if(o=="+")
+{
+    r= Number(a)+Number(b) ;
+    alert (a + " + " + b + " = " + r );
+}
 
+if(o=="-")
+{
+    r= Number(a)-Number(b);
+    alert (a + " - " + b + " = " + r );
+}
+
+if(o=="*")
+{
+    r=a*b;
+    alert (a + " * " + b + " = " + r );
+}
+
+if(o=="/")
+{
+    r=a/b;
+    alert (a + " / " + b + " = " + r );
+}
+}
