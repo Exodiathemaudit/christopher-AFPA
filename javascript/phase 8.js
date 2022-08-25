1aecdd1105076d92e6a7450d9a3cf867192b78b4
Christopher Moreau <moreauchristopher7@gmail.com>
2022-08-25 16:51:31 +0200

    MODIF EXO8.JS

--- javascript/phase 8.js
+++ javascript/phase 8.js
@@ -1,5 +1,15 @@
-let ok = true;
-while (ok)
-{
-    if 
+
+let cpt = 0;
+  
+while(true){
+let N = window.prompt("entrer un prénom");
+  if(N)
+    {
+        N=N;
+        cpt++;
+        
+    }else{
+        break;
+    }
 }
+console.log(cpt++);
