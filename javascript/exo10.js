923d960b521881eaacffa11cf7e75c1699ce29cc
Christopher Moreau <moreauchristopher7@gmail.com>
2022-09-07 16:56:15 +0200

    modif exo 10.js

---
+++ javascript/exo10.js
@@ -0,0 +1,34 @@
+// var myTableau = new Array(7); 
+// myTableau[0] = ["lundi"];
+// myTableau[1] = ["Mardi"];
+// myTableau[2] = ["Mercredi"];
+// myTableau[3] = ["Jeudi"];
+// myTableau[4] = ["Vendredi"];
+// myTableau[5] = ["Samedi"];
+// myTableau[6] = ["dimanche"];
+// var nb = myTableau.length;
+// console.log("le tableau contient"+7+ "élèments");
+// console.log(myTableau);
+
+
+///////////////EXO 1://///////////
+
+let n = window.prompt("saisir la valeur");
+var myTableau = (n);
+var nb = myTableau.length;
+console.log(myTableau);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
