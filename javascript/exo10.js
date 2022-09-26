ac7251b1af48f5afad39261e4f4bae20341e9d96
Christopher Moreau <moreauchristopher7@gmail.com>
2022-09-26 16:53:20 +0200

    modif exo10.js

--- javascript/exo10.js
+++ javascript/exo10.js
@@ -26,63 +26,86 @@
 // ct(myTableau);
 
 ////////////////////////////////////////////////exo 2:///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-let t ;
+// let t ;
 
-function getInteger()
-{
-    let e = Number(window.prompt("entrez un entier"));
-    console.log(e);
+// function getInteger()
+// {
+//     let e = Number(window.prompt("entrez un entier"));
+//     console.log(e);
+// }
+// getInteger();
+
+
+// function InitTab(){
+
+// t = Number(window.prompt("entrez la taille du tableau"));
+// tab = new Array
+// tab.length = t;
+// console.log(tab);
+// }
+// InitTab();
+
+// function saisieTab()
+// {
+
+//     for(let p = 0; p < t; p++ ){
+//     let y= window.prompt("saisie des différents postes");
+//     tab.push(y);
+    
+//     }
+// }
+// saisieTab();
+
+// function afficheTab()
+// {
+//     alert(tab)
+
+    
+// }
+// console.table(tab);
+// afficheTab();
+
+// function rechercheTab()
+// {
+
+// let z =  window.prompt("saisir le numéro de la case");
+// tab.indexOf(z)
+// console.log(tab.indexOf(z));
+// }
+
+// rechercheTab();
+
+
+// function infoTab()
+// {
+// let y 
+
+// }
+
+// infoTab();
+
+
+/////////////////////////////////////////////////////////////////////////exo 3 tri à bulles //////////////////////////////////////////////////////////////////////////////////////////////
+
+function sort(tab){
+let changed;
+do{
+    changed = false;
+
+for(let i=0; i < tab.lenght-1; i++){
+if(tab[i] > tab[i+1]){
+    let tmp = tab[i];
+    tab[i] = tab[i+1];
+    tab[i+1] = tmp;
+    changed = true;
 }
-getInteger();
-
-
-function InitTab(){
-
-t = Number(window.prompt("entrez la taille du tableau"));
-tab = new Array
-tab.length = t;
+}
+}
+while(changed);
+}
+let tab = [5, 8, 11, 6, 1, 9, 3];
+sort(tab);
 console.log(tab);
-}
-InitTab();
-
-function saisieTab()
-{
-  
-    for(let p = 0; p < t; p++ ){
-    let y= window.prompt("saisie des différents postes");
-    tab.push(y);
-    
-    }
-}
-saisieTab();
-
-function afficheTab()
-{
-    alert(tab)
-    
-}
-console.table(tab);
-afficheTab();
-
-function rechercheTab()
-{
-
-let z =  window.prompt("saisir le numéro de la case");
-tab.indexOf(z)
-console.log(tab.indexOf(z));
-}
-
-rechercheTab();
-
-
-function infoTab()
-{
-
-
-}
-
-infoTab();
-
 
 
 
