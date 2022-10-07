8ddffc4a3a714a4ee9281009a4c12185c58f93de
Christopher Moreau <moreauchristopher7@gmail.com>
2022-10-07 12:27:08 +0200

    modif exo 3,4.js

--- javascript/eval_js/exo 3 eval.js
+++ javascript/eval_js/exo 3 eval.js
@@ -6,10 +6,18 @@ nom = window.prompt("entrez le prénom recherché");
 
 if(tab.indexOf(nom) === -1)
 {
-window.prompt("le prénom n'existe pas");
+window.prompt("le prénom n'y est pas");
 }
 else
 {
     pre = tab.indexOf(nom);
-    console.log()
+    console.log("avant tab.splice:" + tab);
+    console.log("pre=" + pre);
+    tab.splice((pre),1);
+    console.log("apres tab.splice:" + tab);
+    tab.push("");
+    console.log("aprés tab.push=" + tab );
+
+window.alert("le prénom: "  + nom + " a été retiré de la liste des prénom:");
+
 }
---
+++ javascript/eval_js/exo 4 eval.js
@@ -0,0 +1,12 @@
+let tot = 0;
+let pu = 0;
+let qte = 0;
+let REM = 0;
+let pap = 0;
+let port = 0;
+let totrem = 0;
+
+pu = window.prompt("Entrez le prix unitaire");
+qte = window.prompt("saisir la quantité");
+
+
