4cd931dc14b54ed8176c540ac9b0a243d8eee619
Christopher Moreau <moreauchristopher7@gmail.com>
2022-10-13 16:44:12 +0200

    modif eval js

--- javascript/eval_js/formulaire eval.js
+++ javascript/eval_js/formulaire eval.js
@@ -181,60 +181,156 @@ let filtreD = new RegExp("^[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$");
 let filtreE = new RegExp("^[a-z0-9]+[.]?[a-z0-9]*@[a-z]+[.][a-z]{2,4}$");
 let filtreQ = new RegExp("^.+");
 
-//Nom
-function verif_nom(n){
 
-    let N = document.getElementById("nom");
+
+let N = document.getElementById("nom");
+let P = document.getElementById("prenom");
+let G = document.getElementById("genre");
+let D = document.getElementById("date de naissance");
+let C = document.getElementById("code postal");
+let A = document.getElementById("adresse");
+let V = document.getElementById("ville");
+let E = document.getElementById("email");
+
+
+
+//Nom
+function verif_nom(N){
 
     if(N == 'null'){
         filtreN;
-        n.preventDefault()
+        N.preventDefault();
     }else{
         filtreN;
         resultat = filtreN.test(N.value);
     }if(resultat == false){
 
-        document.getElementById('1').innerHTML="nom pas conforme";
-        n.preventDefault();
+        document.getElementById('1').innerHTML="nom non conforme";
+        N.preventDefault();
     }
 }
-verif_nom;
+verif_nom(N);
 
 //prenom
-function verif_prenom(P){
+// function verif_prenom(P){
 
-    let P = document.getElementById("prenom");
+//     if(P == 'null'){
+//         filtreP;
+//         P.preventDefault()
 
-    if(P == 'null'){
-        filtreP;
-        P.preventDefault()
+//     }
 
-    }
-
-    else{
-        filtreP;
-        resultat = filtreP.test(P.value);
+//     else{
+//         filtreP;
+//         resultat = filtreP.test(P.value);
 
 
-    }if(resultat == false){
+//     }if(resultat == false){
 
-        document.getElementById('1').innerHTML="prenom pas conforme";
-        P.preventDefault();
-    }
+//         document.getElementById('1').innerHTML="prenom non conforme";
+//         P.preventDefault();
+//     }
 
 
-}
+// }
+// verif_prenom;
 
-//genre
-function verif_HF(hf)
 
-    let hf = document.getElementById("genre");
+// //genre
+// function verif_HF(hf)
 
-    if(hf == 'nul'){
-    filtreHf;
-    resultat = filtreHf.test(hf.value);
+//     let hf = document.getElementById("genre");
 
-    }if(resultat)
+//     if(hf == 'nul'){
+//     filtreHf;
+//     resultat = filtreHf.test(hf.value);
+
+//     }if(resultat == false){
+//         document.getElementById('1').innerHTML="genre non conforme";
+//         hf.preventDefault();
+//     }
+// verif_HF;
+
+
+// //date de naissance
+// function verif_DatedeN(D)
+//     let D = document.getElementById("date de naissance");
+//     if(D == 'nul'){
+//     filtreD;
+//     resultat = filtreD.test(D.value);
+
+// }if(resultat == false){
+//     document.getElementById('1').innerhtml="date de naissance non conforme";
+//     D.preventDefault();
+
+// }
+// verif_DatedeN;
+
+
+// //code postal
+// function verif_codepostal(C)
+//     let c = document.getElementById("code postal");
+//     if(c == 'nul'){
+//     filtreC;
+//     resultat = filtreC.test(c.value);
+
+// }if (resultat == false){
+//     document.getElementById('1').innerHTML="code postal non conforme";
+//     C.preventDefault();
+
+
+// }
+// verif_codepostal;
+
+
+// //adresse
+// function verif_adress(A)
+//     let A = document.getElementById("adresse");
+//     if(A == 'nul'){
+//     filtreA;
+//     resultat = filtreA.test(A.value);
+
+// }if (resultat == false){
+//     document.getElementById('1').innerHTML="adresse non conforme";
+//     A.preventDefault();
+
+// }
+// verif_adress;
+
+
+// //ville
+// function verif_ville(V)
+//     let V = document.getElementById("ville");
+//     if(V == 'nul'){
+//     filtreV;
+//     resultat = filtreV.test(V.value);
+
+// }if (resultat == false){
+//     document.getElementById('1').innerhtml="ville non conforme";
+//     V.preventDefault();
+// }
+// verif_ville;
+
+
+// //email
+// function verif_Email(E)
+//     let E = document.getElementById("email");
+//     if(E == 'nul'){
+//     filtreE;
+//     resultat = filtreE.test(E.value);
+
+// }if (resultat == false){
+//     document.getElementById('1').innerhtml="email non conforme";
+//     E.preventDefault();
+// }
+// verif_Email;
+
+
+
+
+
+
+
 
 
 
---
+++ javascript/eval_js/test1.js
@@ -0,0 +1,9 @@
+get1 = getElementById;
+cl = console.log;
+
+
+alert(document.get1("nom"));
+
+
+// cl('cjkglmjdflgjkdlfjkgldjfklgjkdlgfj')
+
