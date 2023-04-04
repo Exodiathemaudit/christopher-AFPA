2a7f736edf28395bd016194eb82315d822f7b8a8
Exodiathemaudit <moreauchristopher7@gmail.com>
2023-04-04 16:39:27 +0200

    modif exercice sql

--- exo cda/créer_desprocédures_exercice.sql
+++ exo cda/créer_desprocédures_exercice.sql
@@ -1,5 +1,5 @@
 --********************* exercice 1 **********
-
+DELIMITER |
 CREATE Procedure Lst_fournis()
 BEGIN
     SELECT DISTINCT numfou
@@ -13,7 +13,36 @@ CALL Lst_fournis();
 
 SHOW CREATE PROCEDURE Lst_fournis;
 
+DELIMITER;
 
 --******************** EXERCICE 2 **********************
+DELIMITER |
+CREATE PROCEDURE Lst_commandes(IN obs VARCHAR(50) )
+BEGIN
+    SELECT DISTINCT produit.codart, libart, obscom
+    FROM produit
+    JOIN ligcom ON produit.codart = ligcom.codart
+    JOIN entcom ON ligcom.numcom = entcom.numcom
+    WHERE obscom LIKE obs;
+    
+END;
 
-CREATE PROCEDURE lST_Commandes()
+CALL Lst_commandes(%urgent%);
+
+DROP PROCEDURE Lst_commandes;
+
+delimiter;
+
+--***************************** EXERCICE 3 ******************
+DELIMITER |
+
+CREATE Procedure CA_Fournisseur(IN numero INT, annee INT)
+BEGIN
+    SELECT DISTINCT  numero INTO numfou, nomfou, SUM(priuni*qteliv)
+    FROM entcom
+    JOIN ligcom ON entcom.numcom = ligcom.numcom
+    JOIN fournis ON ligcom.numcom = fournis.numfou
+END
+
+DELIMITER ;
+
